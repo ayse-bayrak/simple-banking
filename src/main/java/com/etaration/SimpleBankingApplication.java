e467eea77cd7fe0a4ee9f3fae53437fa6c68c468
ayse-bayrak <aysegunes.bayrak82@gmail.com>
2024-10-13 23:48:40 +0300

    MapperMapper bean is added

--- src/main/java/com/etaration/SimpleBankingApplication.java
+++ src/main/java/com/etaration/SimpleBankingApplication.java
@@ -1,13 +1,34 @@
 package com.etaration;
 
+import com.etaration.entity.BankAccount;
+import com.etaration.repository.BankAccountRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class SimpleBankingApplication {
+public class SimpleBankingApplication implements CommandLineRunner {
+
+    private final BankAccountRepository bankAccountRepository;
+
+    public SimpleBankingApplication(BankAccountRepository bankAccountRepository) {
+        this.bankAccountRepository = bankAccountRepository;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SimpleBankingApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        BankAccount account = new BankAccount("Ayse Gunes Bayrak", "669-7788");
+        bankAccountRepository.save(account);
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 }
