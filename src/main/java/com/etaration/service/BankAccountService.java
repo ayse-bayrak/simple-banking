fccc8b7d53be91d4d1b872c11b073d7c0841df41
ayse-bayrak <aysegunes.bayrak82@gmail.com>
2024-10-14 21:17:35 +0300

    CustomException is added

--- src/main/java/com/etaration/service/BankAccountService.java
+++ src/main/java/com/etaration/service/BankAccountService.java
@@ -1,6 +1,7 @@
 package com.etaration.service;
 
 import com.etaration.dto.AccountResponse;
+import com.etaration.dto.BankAccountDTO;
 import com.etaration.entity.BankAccount;
 
 public interface BankAccountService {
@@ -8,8 +9,7 @@ public interface BankAccountService {
     String credit(String accountNumber, double amount);
     String debit(String accountNumber, double amount);
     AccountResponse getAccountDetails(String accountNumber);
-
-   BankAccount findByAccountNumber(String accountNumber);
-
-    void save(BankAccount account);
+    BankAccount findByAccountNumber(String accountNumber);
+    void save(BankAccountDTO bankAccountDTO);
+    String phoneBillPayment(String accountNumber, String provider, String phoneNumber, Double amount);
 }
