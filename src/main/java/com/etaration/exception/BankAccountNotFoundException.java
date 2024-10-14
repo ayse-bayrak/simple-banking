03a9a37820ca6e41124874bfbdafd9d2b55d32c6
ayse-bayrak <aysegunes.bayrak82@gmail.com>
2024-10-14 22:37:48 +0300

    GlobalExceptionHandler, BankAccountNotFoundException and InsufficientBalanceException are added

---
+++ src/main/java/com/etaration/exception/BankAccountNotFoundException.java
@@ -0,0 +1,10 @@
+package com.etaration.exception;
+
+
+
+public class BankAccountNotFoundException extends RuntimeException{
+
+    public BankAccountNotFoundException(String message) {
+        super(message);
+    }
+}
