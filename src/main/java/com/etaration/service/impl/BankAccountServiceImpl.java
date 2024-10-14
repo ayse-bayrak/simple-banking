a1e1440058165f9bd747c768419574f0454ad1b7
ayse-bayrak <aysegunes.bayrak82@gmail.com>
2024-10-14 22:40:12 +0300

    JavaDocs is added to some method

--- src/main/java/com/etaration/service/impl/BankAccountServiceImpl.java
+++ src/main/java/com/etaration/service/impl/BankAccountServiceImpl.java
@@ -1,20 +1,28 @@
 package com.etaration.service.impl;
 
 import com.etaration.dto.AccountResponse;
+import com.etaration.dto.BankAccountDTO;
 import com.etaration.dto.TransactionDTO;
 import com.etaration.entity.BankAccount;
 import com.etaration.entity.DepositTransaction;
+import com.etaration.entity.PhoneBillPaymentTransaction;
 import com.etaration.entity.WithdrawalTransaction;
+import com.etaration.exception.BankAccountNotFoundException;
+import com.etaration.exception.InsufficientBalanceException;
 import com.etaration.repository.BankAccountRepository;
 import com.etaration.service.BankAccountService;
 import com.etaration.service.TransactionService;
+import com.etaration.util.MapperUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import jakarta.transaction.Transactional;
 
 import java.util.List;
-import java.util.UUID;
+
+/**
+ * Service implementation for handling bank account operations.
+ */
 
 @Slf4j
 @Service
@@ -22,52 +30,102 @@ public class BankAccountServiceImpl implements BankAccountService {
 
     private final BankAccountRepository bankAccountRepository;
     private final TransactionService transactionService;
+    private final MapperUtil mapperUtil;
 
-    public BankAccountServiceImpl(BankAccountRepository bankAccountRepository, @Lazy TransactionService transactionService) {
+    public BankAccountServiceImpl(BankAccountRepository bankAccountRepository, @Lazy TransactionService transactionService, MapperUtil mapperUtil) {
         this.bankAccountRepository = bankAccountRepository;
         this.transactionService = transactionService;
+        this.mapperUtil = mapperUtil;
     }
 
+    /**
+     * Credits a specified amount to the given bank account.
+     *
+     * @param accountNumber the bank account number
+     * @param amount        the amount to be credited
+     * @return the approval code for the transaction
+     * @throws BankAccountNotFoundException if the account is not found
+     */
     @Transactional
     public String credit(String accountNumber, double amount) {
         log.info("Credit process");
         BankAccount account = bankAccountRepository.findByAccountNumber(accountNumber)
-                .orElseThrow(() -> new IllegalArgumentException("Account not found"));
+                .orElseThrow(() -> new BankAccountNotFoundException("Bank Account not found"));
 
         // Create a DepositTransaction and post it to the account
         DepositTransaction depositTransaction = new DepositTransaction(amount);
-
-        // Save the updated account
-        bankAccountRepository.save(account);
+        account.post(depositTransaction);
 
         return transactionService.postTransaction(accountNumber, depositTransaction); // return approvalCode
     }
 
+    /**
+     * Debits a specified amount from the given bank account.
+     *
+     * @param accountNumber the bank account number
+     * @param amount        the amount to be debited
+     * @return the approval code for the transaction
+     * @throws BankAccountNotFoundException if the account is not found
+     * @throws InsufficientBalanceException if balance is insufficient
+     */
     @Transactional
     public String debit(String accountNumber, double amount) {
         log.info("Debit process");
         BankAccount account = bankAccountRepository.findByAccountNumber(accountNumber)
-                .orElseThrow(() -> new IllegalArgumentException("Account not found"));
+                .orElseThrow(() -> new BankAccountNotFoundException("Bank Account not found"));
 
         if (account.getBalance() < amount) {
-            throw new IllegalArgumentException("Insufficient balance");
+            throw new InsufficientBalanceException("Insufficient balance to pay phone bill");
         }
 
         // Create a WithdrawalTransaction and post it to the account
         WithdrawalTransaction withdrawalTransaction = new WithdrawalTransaction(amount);
-        transactionService.postTransaction(accountNumber, withdrawalTransaction);
+        account.post(withdrawalTransaction);
 
-        // Save the updated account
-        bankAccountRepository.save(account);
-
-        return UUID.randomUUID().toString();
+        return transactionService.postTransaction(accountNumber, withdrawalTransaction);
     }
 
+    /**
+     * Processes a phone bill payment from the given bank account.
+     *
+     * @param accountNumber the bank account number
+     * @param provider      the phone service provider
+     * @param phoneNumber   the phone number to be billed
+     * @param amount        the amount to be paid
+     * @return the approval code for the transaction
+     * @throws BankAccountNotFoundException if the account is not found
+     * @throws InsufficientBalanceException if balance is insufficient
+     */
+    @Override
+    @Transactional
+    public String phoneBillPayment(String accountNumber, String provider, String phoneNumber, Double amount) {
+        log.info("Phone Bill Payment process");
+        BankAccount account = bankAccountRepository.findByAccountNumber(accountNumber)
+                .orElseThrow(() -> new BankAccountNotFoundException("Bank Account not found"));
+
+        if (account.getBalance() < amount) {
+            throw new InsufficientBalanceException("Insufficient balance to pay phone bill");
+        }
+
+        // Create a PhoneBillPaymentTransaction and post it to the account
+        PhoneBillPaymentTransaction phoneBillPaymentTransaction = new PhoneBillPaymentTransaction(provider, phoneNumber, amount);
+        account.post(phoneBillPaymentTransaction);
+
+        return transactionService.postTransaction(accountNumber, phoneBillPaymentTransaction);
+    }
+
+    /**
+     * Retrieves details of the given bank account, including its transaction history.
+     *
+     * @param accountNumber the bank account number
+     * @return an {@link AccountResponse} containing account details and transactions
+     * @throws BankAccountNotFoundException if the account is not found
+     */
     @Override
     public AccountResponse getAccountDetails(String accountNumber) {
         log.info("getAccountDetails are retrieving.");
         BankAccount account = bankAccountRepository.findByAccountNumber(accountNumber)
-                .orElseThrow(() -> new IllegalArgumentException("Account not found"));
+                .orElseThrow(() -> new BankAccountNotFoundException("Bank Account not found"));
 
         List<TransactionDTO> transactionDTOS = transactionService.getTransactionsByAccountNumber(accountNumber);
 
@@ -76,21 +134,33 @@ public class BankAccountServiceImpl implements BankAccountService {
                 .owner(account.getOwner())
                 .balance(account.getBalance())
                 .createDate(account.getCreatedDate())
-                .transactions(transactionDTOS) // Set populated transaction list
+                .transactions(transactionDTOS)
                 .build();
     }
 
+    /**
+     * Finds a bank account by its account number.
+     *
+     * @param accountNumber the bank account number
+     * @return the {@link BankAccount} entity
+     * @throws BankAccountNotFoundException if the account is not found
+     */
     @Override
     public BankAccount findByAccountNumber(String accountNumber) {
         log.info("Account is searching in BankAccountRepository");
         return bankAccountRepository.findByAccountNumber(accountNumber)
-                .orElseThrow(() -> new IllegalArgumentException("Account not found"));
+                .orElseThrow(() -> new BankAccountNotFoundException("Bank Account not found"));
     }
 
+    /**
+     * Saves a new bank account to the repository.
+     *
+     * @param bankAccountDTO the {@link BankAccountDTO} to be saved
+     */
     @Override
-    public void save(BankAccount account) {
+    public void save(BankAccountDTO bankAccountDTO) {
         log.info("Account is saved to BankRepository");
-        bankAccountRepository.save(account);
+        bankAccountRepository.save(mapperUtil.convert(bankAccountDTO, new BankAccount()));
     }
 
 
--- src/main/java/com/etaration/service/impl/TransactionServiceImpl.java
+++ src/main/java/com/etaration/service/impl/TransactionServiceImpl.java
@@ -1,10 +1,9 @@
 package com.etaration.service.impl;
 
+import com.etaration.dto.BankAccountDTO;
 import com.etaration.dto.TransactionDTO;
 import com.etaration.entity.BankAccount;
-import com.etaration.entity.DepositTransaction;
 import com.etaration.entity.Transaction;
-import com.etaration.entity.WithdrawalTransaction;
 import com.etaration.repository.TransactionRepository;
 import com.etaration.service.BankAccountService;
 import com.etaration.service.TransactionService;
@@ -17,6 +16,9 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
+/**
+ * Service implementation for handling transactions related to bank accounts.
+ */
 @Service
 @Slf4j
 public class TransactionServiceImpl implements TransactionService {
@@ -31,6 +33,13 @@ public class TransactionServiceImpl implements TransactionService {
         this.bankAccountService = bankAccountService;
     }
 
+    /**
+     * Posts a transaction to the specified bank account and generates an approval code.
+     *
+     * @param accountNumber the bank account number
+     * @param transaction   the transaction object (e.g., Deposit, Withdrawal)
+     * @return the approval code for the transaction
+     */
     @Override
     @Transactional
     public String postTransaction(String accountNumber, Transaction transaction) {
@@ -38,34 +47,34 @@ public class TransactionServiceImpl implements TransactionService {
         BankAccount account = bankAccountService.findByAccountNumber(accountNumber);
         String approvalCode = UUID.randomUUID().toString();
         transaction.setDate(LocalDateTime.now()); // Set the current date and time
-        transaction.setBankAccountNumber(accountNumber);
+        transaction.setBankAccount(account);
+        transaction.setType(transaction.getType());
         transaction.setApprovalCode(approvalCode);
-        transaction.setStatus("OK");
-        double balance = account.getBalance();
-        if (transaction instanceof DepositTransaction) {
-            balance += transaction.getAmount();
-        } else if (transaction instanceof WithdrawalTransaction) {
-            if (balance >= transaction.getAmount()) {
-                balance -= transaction.getAmount();
-            } else {
-                throw new IllegalArgumentException("Insufficient funds for withdrawal");
-            }
-        }
-        account.setBalance(balance);
         transactionRepository.save(transaction);
-        bankAccountService.save(account);
+        bankAccountService.save(mapperUtil.convert(account, new BankAccountDTO()));
         return approvalCode;
     }
 
+    /**
+     * Retrieves the list of transactions for a specific bank account by account number.
+     *
+     * @param accountNumber the bank account number
+     * @return a list of {@link TransactionDTO} representing the transactions for the account
+     */
     @Override
     public List<TransactionDTO> getTransactionsByAccountNumber(String accountNumber) {
-        log.info("all transaction "+ transactionRepository.findAllByBankAccountNumber(accountNumber));
-        return transactionRepository.findAllByBankAccountNumber(accountNumber)
+        BankAccount account = bankAccountService.findByAccountNumber(accountNumber);
+        log.info("all transaction "+ transactionRepository.findAllByBankAccount(account));
+        return transactionRepository.findAllByBankAccount(account)
                 .stream().map(transaction -> mapperUtil.convert(transaction, new TransactionDTO()))
                 .toList();
-
     }
 
+    /**
+     * Saves the provided transaction to the repository.
+     *
+     * @param transaction the transaction entity to be saved
+     */
     @Override
     public void save(Transaction transaction) {
         transactionRepository.save(transaction);
